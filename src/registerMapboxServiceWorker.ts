1cb71ce324e91fac019392d626e4576c207efa5d
Anthony Penna <penna1296@gmail.com>
2021-12-09 16:28:24 +0100

    chore: cache tiles only in production

--- src/registerMapboxServiceWorker.ts
+++ src/registerMapboxServiceWorker.ts
@@ -1,6 +1,6 @@
-const serviceWorkerPath = 'mapboxsw.js'
+const serviceWorkerPath = '/mapboxsw.js'
 
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   window.addEventListener('load', async () => {
     await navigator.serviceWorker.register(serviceWorkerPath)
     console.log('Registered mapbox service worker.')
