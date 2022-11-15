f9f6a50d83cdc341d3fc4a63c2d5e5ddf4971d55
Anthony Penna <penna1296@gmail.com>
2022-11-15 13:28:59 +0100

    chore(eslint config): remove unneeded check

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
     'no-undef': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
